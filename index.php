8157cbd8e438a28a142616649b634582d5a62c44
Aishwarya Karanth <35144851+AishwaryaKaranth@users.noreply.github.com>
2020-02-01 22:45:19 +0530

    added pdf download feature

--- index.php
+++ index.php
@@ -7,11 +7,14 @@
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.0.272/jspdf.debug.js"></script>
    <link href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <style>
         div{
             font:15px Verdana;
-            width:450px;
+            
         }
         ul {
             padding:0;
@@ -27,55 +30,105 @@
             display:inline-block; 
         }
         table, input {
-            text-align:left;
+            text-align:center;
             font:13px Verdana;
         }
         table, td, th 
         {
             margin:10px 0;
-            padding:2px 10px;
+            padding:2px 60px;
         }
         td, th {
-            border:solid 1px #CCC;
+            border:solid 3px #CCC;
+            border-color: ;
         }
         button {
             font:13px Verdana;
             padding:3px 5px;
         }
-        table.dataTable thead .sorting:after,
-table.dataTable thead .sorting:before,
-table.dataTable thead .sorting_asc:after,
-table.dataTable thead .sorting_asc:before,
-table.dataTable thead .sorting_asc_disabled:after,
-table.dataTable thead .sorting_asc_disabled:before,
-table.dataTable thead .sorting_desc:after,
-table.dataTable thead .sorting_desc:before,
-table.dataTable thead .sorting_desc_disabled:after,
-table.dataTable thead .sorting_desc_disabled:before {
-bottom: .5em;
+        
+        #bg{
+           /* background:linear-gradient(#11998e,#38ef7d);*/
+            width:100%;
+           height:50%;
+            
+        }
+        #sign{
+            width:50%;
+            position:relative;
+            left:30%;
+            padding-top:30px;
+            background: linear-gradient(#a8ff78, #78ffd6);
+       }
+       #header{
+        text-align: center;
+       }
+       
+        
+        h1{
+            position: relative;
+        }
+        span{
+            /*background: linear-gradient(#8e9eab, #eef2f3);*/
+
+        }
+        
+
 }
     </style>
 </head>
 <body>
     <div ng-app="myApp" ng-controller="myController">
-        <form method="post">
-            <input type="submit" name="generate_pdf" class="btn btn success" value="Generate pdf">
-        </form>
+        <div id="header">
+            <h1>TEQUED LABS</h1>
+        </div>
+        <h3>Student Information Portal</h3>
         
-           Sort by: <select><option value="alphabetic_order" onclick="sortByAlphabeticOrder()">Alphabetic order</option>
-            <option value="college" onclick="sortByCollege()">College</option>
-            <option value="course" onclick="sortByCourse()">Course</option>
-            <option value="instructor" onclick="sortByInstructor()">Instructor</option>
-            <option value="year_of_graduation" onclick="sortByYear()">Year</option>
+        <div id="sign">
+        <div class="input-group input-group-sm mb-3">
+        <div class="input-group-prepend">
+        <span class="input-group-text" id="inputGroup-sizing-sm">Name</span>
+        </div>
+        <input type="text" ng-model="name" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+        </div>
+        
 
-           </select>
-       
-        <ul>
-            <li> Name</li>
-           <li> <input type="text" ng-model="name" /></li>
-        </ul>
-        <ul>
-            <li>college</li>
+        <div class="input-group input-group-sm mb-3">
+        <div class="input-group-prepend">
+        <span class="input-group-text" id="inputGroup-sizing-sm">College</span>
+        </div>
+        <input type="text" ng-model="college" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+        </div>
+
+        <div class="input-group input-group-sm mb-3">
+        <div class="input-group-prepend">
+        <span class="input-group-text" id="inputGroup-sizing-sm">Course</span>
+        </div>
+        <input type="text" ng-model="course" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+        </div>
+        
+        <div class="input-group input-group-sm mb-3">
+        <div class="input-group-prepend">
+        <span class="input-group-text" id="inputGroup-sizing-sm">Year of graduation</span>
+        </div>
+        <input type="text" ng-model="year_of_graduation" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+        </div>
+            
+       <div class="input-group input-group-sm mb-3">
+       <div class="input-group-prepend">
+       <span class="input-group-text" id="inputGroup-sizing-sm">Instructor</span>
+       </div>
+       <input type="text" ng-model="instructor" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+       </div>
+
+        <div class="input-group input-group-sm mb-3">
+        <div class="input-group-prepend">
+        <span class="input-group-text" id="inputGroup-sizing-sm">Fee Paid</span>
+        </div>
+        <input type="text" ng-model="fee_paid" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+        </div>
+        </div>
+     <!--       <li>college</li>
            <li> <input type="text" ng-model="college" /></li>
         </ul>
         <ul>
@@ -94,33 +147,25 @@ bottom: .5em;
             <li>fee paid</li>
            <li> <input type="number" ng-model="fee_paid" /></li>
         </ul>
-        <ul>
-            <li> </li>
-            <input type="submit" ng-click="submit()">Submit Data/>
+        <ul>-->
+            
+            <input type="submit" ng-click="submit()">
 
-            <li><button ng-click="addRow()"> Add Row </button></li>
+          <!--  <li><button ng-click="addRow()"> Add Row </button></li>-->
         </ul>
 
         <!--CREATE A TABLE-->
-        <div id="content">
+        
         <table   id="selectedColumn" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%"> 
           
                
         
-        <div>      
-                <button ng-click="removeRow()">Remove Row</button>
-        </div>-->
+       
        
     </table>
-    <form action ="submit.php" method="post">  
-                          <input type="submit" name="create_pdf" class="btn btn-danger" value="Create PDF" />  
-                     </form>
-</div>
-    <div id="editor"></div>
-    <a href="#" id="cmd">generate pdf</a>
-        <div id="display" style="padding:10px 0;">{{display}}</div>
-    </div>
     
+
+    <button type="button" id="pdfDownloader" class="btn btn-primary">Download</button>
 </body>
 </html>
 <!--The Controller-->
@@ -199,17 +244,28 @@ bottom: .5em;
             
         }
     });
+    $(document).ready(function(){
  
-    $(document).ready(function () {
-$('#selectedColumn').DataTable({
-  "aaSorting": [],
-  columnDefs: [{
-  orderable: false,
-  targets: 3
-  }]
-});
-  $('.dataTables_length').addClass('bs-select');
-});
+    $("#pdfDownloader").click(function(){
+    
+        html2canvas(document.getElementById("content"), {
+            onrendered: function(canvas) {
+
+                var imgData = canvas.toDataURL('image/png');
+                console.log('Report Image URL: '+imgData);
+                var doc = new jsPDF('p', 'mm', [297, 210]); //210mm wide and 297mm high
+                
+                doc.addImage(imgData, 'PNG', 10, 10);
+                doc.save('sample.pdf');
+            }
+        });
+
+    });
+    
+    
+})
+
+ 
 </script>
 
 <?php
@@ -242,7 +298,7 @@ $('#selectedColumn').DataTable({
         $sort=='DESC' ? $sort='ASC':$sort='DESC';
 
     echo "
-    <table border='1'>
+    <table id='content' border='1'>
     <tr>
     <th><a href='?order=name && sort=$sort'>Name</a></th>
     <th><a href='?order=college && sort=$sort'>College</a></th>
@@ -282,7 +338,7 @@ else{
 
 
 ?>
-    <script>
+    <!--<script>
          $(document).ready(function(){
         var doc=new jsPDF();
         var specialElementHandlers={
@@ -298,4 +354,4 @@ else{
     },2000);
     });
     });
-    </script>
+    </script>-->
